a8712cc4a0273f3107bf52d635d0ed66e0c643e9
Gomez Martin <daniel.gomez@realtech.es>
2019-09-12 10:55:53 +0200

    Primer test deploy AWS

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM gcr.io/google-appengine/nodejs	
-EXPOSE 8080
-COPY . /app/
-RUN npm install
-RUN npm run build
-ENTRYPOINT npm run start:prod
-
--- deployment.sh
+++
@@ -1,6 +0,0 @@
-# !/bin/bash
-set -e
-echo $ACCOUNT_KEY_STAGING > service_key.txt
-base64 -i service_key.txt -d > ${HOME}/gcloud-service-key.json
-gcloud auth activate-service-account ${ACCOUNT_ID} --key-file ./gcloud-service-key.json
-gcloud app deploy
